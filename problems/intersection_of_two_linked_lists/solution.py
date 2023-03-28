afe30089353013816afa78e8b1ca03afc09819b9
Vikrom Narula <vikrom.nar@gmail.com>
2023-03-28 03:18:20 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (python3)

---
+++ problems/intersection_of_two_linked_lists/solution.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        visited = set()
+        while headA:
+            visited.add(headA)
+            headA = headA.next
+        while headB:
+            if headB in visited:
+                return headB
+            else:
+                visited.add(headB)
+                headB = headB.next
+        return None
+
+"""
+Space: O(1) Time: O(n+m)
+class EditorialSolution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pA = headA
+        pB = headB
+
+        while pA != pB:
+            pA = headB if pA is None else pA.next
+            pB = headA if pB is None else pB.next
+
+        return pA
+        # Note: In the case lists do not intersect, the pointers for A and B
+        # will still line up in the 2nd iteration, just that here won't be
+        # a common node down the list and both will reach their respective ends
+        # at the same time. So pA will be NULL in that case.
+"""
