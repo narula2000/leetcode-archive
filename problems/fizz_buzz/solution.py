cc59136556c8e7b30630143f79a457739149f5f1
Vikrom Narula <vikrom.nar@gmail.com>
2023-04-09 01:45:03 +0000

    Sync LeetCode submission - Fizz Buzz (python3)

---
+++ problems/fizz_buzz/solution.py
@@ -0,0 +1,45 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for i in range(1,n+1):
+            if i % 15 == 0:
+                answer.append('FizzBuzz')
+            elif i % 5 == 0:
+                answer.append('Buzz')
+            elif i % 3 == 0:
+                answer.append('Fizz')
+            else:
+                answer.append(str(i))
+        return answer
+
+
+"""
+class EditorialSolution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        # ans list
+        ans = []
+
+        # Dictionary to store all fizzbuzz mappings
+        fizz_buzz_dict = {3 : "Fizz", 5 : "Buzz"}
+        
+        # List of divisors which we will iterate over.
+        divisors = [3, 5]
+
+        for num in range(1, n + 1):
+
+            num_ans_str = []
+
+            for key in divisors:
+                # If the num is divisible by key,
+                # then add the corresponding string mapping to current num_ans_str
+                if num % key == 0:
+                    num_ans_str.append(fizz_buzz_dict[key])
+
+            if not num_ans_str:
+                num_ans_str.append(str(num))
+
+            # Append the current answer str to the ans list
+            ans.append(''.join(num_ans_str))
+
+        return ans
+"""
