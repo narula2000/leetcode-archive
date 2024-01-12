a77651dfb86b45e71e11c83c0119080dd44046a7
Vikrom Narula <vikrom.nar@gmail.com>
2024-01-12 03:32:40 +0000

    Sync LeetCode submission - Determine if String Halves Are Alike (python3)

--- problems/determine_if_string_halves_are_alike/solution.py
+++ problems/determine_if_string_halves_are_alike/solution.py
@@ -1,18 +1,10 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        vowels = "aeiouAEIOU"
-        middle = int(len(s) / 2)
-        a = s[:middle]
-        b = s[middle:]
-
-        counterA = 0
-        for char in a:
-            if char in vowels:
-                counterA += 1
-        counterB = 0
-        for char in b:
-            if char in vowels:
-                counterB += 1
-
-        return counterA == counterB
+        mid = len(s) // 2
+        a, b = s[:mid], s[mid:]
+        return self.countVowels(a) == self.countVowels(b)
         
+    
+    def countVowels(self, string):
+        vowels = set("aeiou")
+        return sum(1 for char in string.lower() if char in vowels)
