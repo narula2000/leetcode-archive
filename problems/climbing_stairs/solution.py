c857780c2476fef5fd24522ef42ee802563f3573
Vikrom Narula <vikrom.nar@gmail.com>
2024-01-18 01:56:13 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- problems/climbing_stairs/solution.py
+++ problems/climbing_stairs/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        hash = dict()
-        hash[1] = 1
-        hash[2] = 2
-        def helper(num):
-            if num in hash:
-                return hash[num]
-            else:
-                hash[num] = helper(num-1) + helper(num-2)
-                return hash[num]
-        return helper(n)
+        memo = [0 for _ in range(n+1)]
+        memo[0], memo[1] = 1, 1
+        for i in range(2, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        
+        return memo[n]
