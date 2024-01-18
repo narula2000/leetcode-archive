59b20dc1e7abc435ec260e5648798af48bd4b712
Vikrom Narula <vikrom.nar@gmail.com>
2024-01-18 03:46:56 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- problems/top_k_frequent_elements/solution.py
+++ problems/top_k_frequent_elements/solution.py
@@ -1,37 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = dict()
-        for num in nums:
-            if num in counter:
-                counter[num] += 1
-            else:
-                counter[num] = 1
+        count = Counter(nums)
+        N = len(nums)
+        freqs = [[] for _ in range(N+1)]
+
+        for num, count in count.items():
+            freqs[count].append(num) # Aggregate all number of that freq
+
+        ans = [] 
+        for i in range(N, 0, -1):
+            for num in freqs[i]:
+                ans.append(num)
+                if len(ans) == k:
+                    return ans
+
+
         
-        maxs = list(counter.values())
-        maxs.sort(reverse=True)
-
-        k_s = set(maxs[:k])
-
-        result = []
-        for key, val in counter.items():
-            if val in k_s:
-                result.append(key)
-
-        return result
-
-"""
-from collections import Counter
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
-        # O(1) time 
-        if k == len(nums):
-            return nums
-        
-        # 1. build hash map : character and how often it appears
-        # O(N) time
-        count = Counter(nums)   
-        # 2-3. build heap of top k frequent elements and
-        # convert it into an output array
-        # O(N log k) time
-        return heapq.nlargest(k, count.keys(), key=count.get) 
-"""
