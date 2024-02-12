941c5445421366fd71b4dd17bea0a5e841a09209
Vikrom Narula <vikrom.nar@gmail.com>
2024-02-12 03:13:09 +0000

    Sync LeetCode submission - Majority Element (python3)

--- problems/majority_element/solution.py
+++ problems/majority_element/solution.py
@@ -1,25 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hash = dict()
-        for num in nums:
-            if num in hash:
-                hash[num] += 1
-            else:
-                hash[num] = 1
-        for key, val in hash.items():
-            if val > len(nums) / 2:
-                return key
-
-"""
-class EditorialSolution:
-    def majorityElement(self, nums):
-        count = 0
-        candidate = None
-
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-
-        return candidate
-"""
+        curr = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            count += (1 if curr == nums[i] else -1)
+            if not count:
+                curr = nums[i]
+                count = 1
+        return curr
