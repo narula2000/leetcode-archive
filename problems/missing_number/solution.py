5538074a0a383e6b953613208e8c1d95d54fb069
Vikrom Narula <vikrom.nar@gmail.com>
2024-02-20 03:07:24 +0000

    Sync LeetCode submission - Missing Number (python3)

--- problems/missing_number/solution.py
+++ problems/missing_number/solution.py
@@ -1,6 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        _nums = set(nums)
         for i in range(len(nums)+1):
-            if i not in nums:
+            if i not in _nums:
                 return i
         return -1
