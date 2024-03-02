5d83994390f11ddf4b216948801271eb051934a1
Vikrom Narula <vikrom.nar@gmail.com>
2024-03-02 14:34:18 +0000

    Sync LeetCode submission - Minimum Operations to Exceed Threshold Value I (python3)

---
+++ problems/minimum_operations_to_exceed_threshold_value_i/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        cnt = 0
+        for num in nums:
+            if num < k:
+                cnt += 1
+        return cnt
