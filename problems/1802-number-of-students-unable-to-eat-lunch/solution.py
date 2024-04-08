c123d79da05a11dded0e18df96500832b08c987b
Vikrom Narula <vikrom.nar@gmail.com>
2024-04-08 03:58:08 +0000

    Sync LeetCode submission Runtime - 39 ms (61.23%), Memory - 16.5 MB (95.31%)

---
+++ problems/1802-number-of-students-unable-to-eat-lunch/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        res = len(students)
+        count = Counter(students)
+
+        for sandwich in sandwiches:
+            if count[sandwich] > 0:
+                res -= 1
+                count[sandwich] -= 1
+            else:
+                return res
+
+        return res
