5009f08a43511702b1a418c6a2945b2182261c0f
Vikrom Narula <vikrom.nar@gmail.com>
2024-04-24 01:47:46 +0000

    Sync LeetCode submission Runtime - 36 ms (46.61%), Memory - 16.6 MB (32.46%)

---
+++ problems/1236-n-th-tribonacci-number/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n < 2:
+            return 1
+        memo = [0] * (n+1)
+        memo[1] = 1
+        memo[2] = 1
+        for i in range(3, n+1):
+            memo[i] = memo[i-1] + memo[i-2] + memo[i-3]
+        return memo[n]
+
