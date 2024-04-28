718aba410681ff07f5e4557ae551e5e740f93e10
Vikrom Narula <vikrom.nar@gmail.com>
2024-04-28 03:52:16 +0000

    Sync LeetCode submission Runtime - 826 ms (31.16%), Memory - 47.2 MB (24.78%)

---
+++ problems/0863-sum-of-distances-in-tree/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+  def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+    ans = [0] * n
+    count = [1] * n
+    tree = collections.defaultdict(set)
+
+    for u, v in edges:
+      tree[u].add(v)
+      tree[v].add(u)
+
+    def postorder(node, parent=None):
+      for child in tree[node]:
+        if child == parent:
+          continue
+        postorder(child, node)
+        count[node] += count[child]
+        ans[node] += ans[child] + count[child]
+
+    def preorder(node, parent=None):
+      for child in tree[node]:
+        if child == parent:
+          continue
+        ans[child] = ans[node] - count[child] + (n - count[child])
+        preorder(child, node)
+
+    postorder(0)
+    preorder(0)
+    return ans 
