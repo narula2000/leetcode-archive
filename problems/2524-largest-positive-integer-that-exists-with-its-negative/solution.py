4a60460e75f4ae257fd355ffe9fc74af7127d37a
Vikrom Narula <vikrom.nar@gmail.com>
2024-05-02 03:24:54 +0000

    Sync LeetCode submission Runtime - 112 ms (35.71%), Memory - 16.7 MB (75.17%)

---
+++ problems/2524-largest-positive-integer-that-exists-with-its-negative/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findMaxK(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        for i in range(len(nums)):
+            if nums[i] > 0 and -nums[i] in nums:
+                return nums[i]
+        return -1
