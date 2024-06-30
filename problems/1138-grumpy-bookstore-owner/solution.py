fc42f170c2dee5815f09fd82710dcffd0a260508
Vikrom Narula <vikrom.nar@gmail.com>
2024-06-30 01:31:21 +0000

    Sync LeetCode submission Runtime - 226 ms (22.10%), Memory - 18.9 MB (61.70%)

--- problems/1138-grumpy-bookstore-owner/solution.py
+++ problems/1138-grumpy-bookstore-owner/solution.py
@@ -1,21 +1,17 @@
-class Solution(object):
-    def maxSatisfied(self, customers, grumpy, minutes):
-        initial_satisfaction = 0
-        max_extra_satisfaction = 0
-        current_window_satisfaction = 0
-        
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        ans = 0
+        total = sum((1 - grumpy[i]) * customers[i] for i in range(len(customers)))
+
+        window_all = 0
+        window_partial = 0
         for i in range(len(customers)):
-            if grumpy[i] == 0:
-                initial_satisfaction += customers[i]
-            elif i < minutes:
-                current_window_satisfaction += customers[i]
-        
-        max_extra_satisfaction = current_window_satisfaction
-        
-        for i in range(minutes, len(customers)):
-            current_window_satisfaction += customers[i] * grumpy[i]
-            current_window_satisfaction -= customers[i - minutes] * grumpy[i - minutes]
-            max_extra_satisfaction = max(max_extra_satisfaction, current_window_satisfaction)
-        
-        return initial_satisfaction + max_extra_satisfaction
-         
+            window_all += customers[i]
+            window_partial += (1 - grumpy[i]) * customers[i]
+            if i + 1 >= minutes:
+                ans = max(ans, total - window_partial + window_all)
+                left = i - minutes + 1
+                window_all -= customers[left]
+                window_partial -= (1 - grumpy[left]) * customers[left]
+
+        return ans 
